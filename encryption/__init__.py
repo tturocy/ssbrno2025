3f23741fd0a277a4ca30ebbb3a2b018b8502b272
bk360 <boonhan.koh@gmail.com>
2025-09-02 16:20:21 +0200

    Extend lookup table to entire set of uppercase Roman alphabets

--- encryption/__init__.py
+++ encryption/__init__.py
@@ -1,3 +1,5 @@
+import string
+
 from otree.api import *
 
 
@@ -20,13 +22,13 @@ class Subsession(BaseSubsession):
 
     def setup_round(self):
         self.payment_per_correct = Currency(0.10)
-        self.lookup_table = "BA"
+        self.lookup_table = string.ascii_uppercase
         self.word = "AB"
 
     @property
     def lookup_dict(self):
         lookup = {}
-        for letter in ["A", "B"]:
+        for letter in string.ascii_uppercase:
             lookup[letter] = self.lookup_table.index(letter)+1
         return lookup
 
